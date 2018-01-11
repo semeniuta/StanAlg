0c4108dc86271e257c8d91794ea4715073b6cf7a
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2018-01-11 17:05:25 +0100

    Better organization of playwithheap.cpp

--- spikes/playwithheap.cpp
+++ spikes/playwithheap.cpp
@@ -12,19 +12,25 @@ int main() {
         my_heap.printIndices();
     }
 
-    std::cout << "Trying to remove number 13 \n";
-    auto idx_of_num = my_heap.findIndex(13);
+    std::cout << "\n# Removing element by value demo\n";
 
-    std::cout << "Number 13 is at index " << idx_of_num << ". ";
+    int number_to_remove = 4;
+    std::cout << "Trying to remove number " << number_to_remove << "\n";
+    auto idx_of_num = my_heap.findIndex(number_to_remove);
+
+    std::cout << "Number 4 is at index " << idx_of_num << ". ";
     if (idx_of_num > 0) {
         std::cout << "Removing it\n";
-        my_heap.remove( (unsigned long)idx_of_num );
-        my_heap.printData();
+        if (idx_of_num >= 0) {
+            my_heap.remove( (unsigned long)idx_of_num );
+            my_heap.printData();
+        }
     } else {
         std::cout << "\n";
     }
 
-    std::cout << "Popping ... \n";
+    std::cout << "# Popping heap root demo\n";
+
     while (my_heap.size() > 0) {
         std::cout << my_heap.pop() << std::endl;
         my_heap.printData();
